937dd43ae4747e79ba44b136d348a588ef19da5e
jessi ortega <ortegabx05@gmail.com>
2019-07-22 03:50:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,8 @@ end
 
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter seasonarray = holiday_hash[:winter].map do |holiday, items|
+  # return an array of all of the supplies that are used in the winter season
+  array = holiday_hash[:winter].map do |holiday, items|
     items
   end
   array.flatten
